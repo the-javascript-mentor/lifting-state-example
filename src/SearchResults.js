3613eb0fc6885c429022f06cd27b96b49364cbe4
Bence A. Tóth <tothab@gmail.com>
2021-09-30 10:46:14 +0200

    Rendered search results

--- src/SearchResults.js
+++ src/SearchResults.js
@@ -1,9 +1,37 @@
+import { useEffect, useState } from "react";
+
 const SearchResults = ({ searchQuery }) => {
+  const [searchResults, setSearchResults] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://api.github.com/search/users?q=${searchQuery}`)
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.items) {
+          setSearchResults(data.items);
+        }
+      });
+  }, [searchQuery]);
+
   return (
     <main>
-      {searchQuery === ""
-        ? "No search"
-        : `You have searched for ${searchQuery}`}
+      {searchQuery === "" ? (
+        "No search"
+      ) : (
+        <div>
+          <p>You have searched for {searchQuery}</p>
+          {searchResults.map((searchResult) => (
+            <div>
+              <img
+                style={{ maxHeight: 120 }}
+                src={searchResult.avatar_url}
+                alt={searchResult.login}
+              />
+              {searchResult.login}
+            </div>
+          ))}
+        </div>
+      )}
     </main>
   );
 };
